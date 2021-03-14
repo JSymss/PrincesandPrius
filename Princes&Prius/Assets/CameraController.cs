064d1ade96dcf6fc139f3da63212a2dda75d6e0f
Jackson <200341@student.sae.edu.au>
2021-03-14 12:51:55 +1100

    Car assets and physics implemented

---
+++ Princes&Prius/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    int DistanceAway = 10;
+    public float yDist = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector3 PlayerPOS = GameObject.Find("Prius").transform.transform.position;
+        GameObject.Find("MainCamera").transform.position = new Vector3(PlayerPOS.x, PlayerPOS.y + yDist, PlayerPOS.z - DistanceAway);
+    }
+}
